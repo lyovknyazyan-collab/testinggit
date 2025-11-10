e1e5c80883bff842cf8ebf677e199ba1e1c45c6c
lyovknyazyan-collab <lyovknyazyan@gmail.com>
2025-11-10 16:33:49 +0400

    Initial Commit

---
+++ testinggit/testinggitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testinggitApp.swift
+//  testinggit
+//
+//  Created by LK on 10.11.25.
+//
+
+import SwiftUI
+
+@main
+struct testinggitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
